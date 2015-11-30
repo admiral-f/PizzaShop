dc092d5c079982cc9f4835bb092e5d18e51c92c7
admiral-f <admiral-f@yandex.ru>
2015-11-30 19:03:33 +0300

    add contacts view and pony sender email

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pony'
 
 get '/' do
 	erb :index			
@@ -9,4 +10,35 @@ end
 
 get '/about' do
 	erb :about			
+end
+
+get '/contacts' do
+	erb :contacts			
+end
+
+post '/contacts' do
+	@name= params[:name]
+	@email = params[:email]
+	@text = params[:text]
+	hh={:name=> "Please, enter your name", :email=>"Please, enter your phone Email", :text=>"Enter your text-message"}
+	@error=hh.select {|key,_| params[key]==""}.values.join("; ")
+	if @error==''
+    	Pony.mail(
+      		:to => "admiral-f@yandex.ru",
+      		:from => "admiral-f@yandex.ru",
+      		:subject => params[:name] + " has contacted you",
+      		:body => params[:name] + " write message: " + params[:text],
+      		:via => :smtp,
+      		:via_options => { 
+        	:address              => 'smtp.yandex.ru', 
+        	:port                 => '465', 
+        	:enable_starttls_auto => true, 
+        	:user_name            => 'admiral-f', 
+        	:password             => 'kalinin1987', 
+        	:authentication       => :plain
+    		})
+    		@answer = 'Your message send'
+  	end  
+  erb :contacts
+			
 end
