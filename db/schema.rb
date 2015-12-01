efba9fb4c66060653fa4af48c09a3de36e51c834
admiral-f <admiral-f@yandex.ru>
2015-12-01 22:14:01 +0300

    add data to database with add_products migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201181126) do
+ActiveRecord::Schema.define(version: 20151201183546) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
-    t.text     "discription"
+    t.text     "description"
     t.decimal  "price"
     t.decimal  "size"
     t.boolean  "is_spicy"
