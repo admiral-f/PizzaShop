ce872995a6250de0784b9765c3815423b787e69c
admiral-f <admiral-f@yandex.ru>
2015-12-03 21:00:40 +0300

    update add_to_card function

--- public/app.js
+++ public/app.js
@@ -1,14 +1,10 @@
-	function something()
-	{
-		var x = window.localStorage.getItem('aaa');
-		x= x*1 + 1;
-		window.localStorage.setItem('aaa', x);
-		alert(x); 
-	}
+function add_to_cart(id)
+{
+	var key = 'product_' + id;
+	var x=window.localStorage.getItem(key);
+	x = x * 1 + 1;
+	window.localStorage.setItem(key, x);
+}
 
-	function add_to_cart()
-	{
-		alert('Hello ooo');
-	}
 
 
