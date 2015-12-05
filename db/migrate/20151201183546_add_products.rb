def50b80d43cbace1a9d7e6fc67d22d3b10b5158
admiral-f <admiral-f@yandex.ru>
2015-12-05 19:41:17 +0300

    add data product to database

--- db/migrate/20151201183546_add_products.rb
+++ db/migrate/20151201183546_add_products.rb
@@ -73,6 +73,36 @@ class AddProducts < ActiveRecord::Migration
       })
 
     Product.create ({
+      :category => 'salad',
+      :title => 'Greek salad', 
+      :description => 'Choriatiki or Horiatiki salad, or Greek Salad as it is known worldwide, means country or village salad, following the meaning of the word “horiatiki” in Greek', 
+      :price => 2, 
+      :is_veg => true, 
+      :is_best_offer => true, 
+      :path_to_image => '/img/greek_salad.jpg'
+      })
+
+    Product.create ({
+      :category => 'salad',
+      :title => 'Cesar salad', 
+      :description => 'Ave, Cesar!!!', 
+      :price => 2, 
+      :is_veg => true, 
+      :is_best_offer => false, 
+      :path_to_image => '/img/cesar_salad.jpg'
+      })
+
+    Product.create ({
+      :category => 'salad',
+      :title => 'Fruit salad', 
+      :description => 'Strawberries, blueberries, kiwi, milk', 
+      :price => 2, 
+      :is_veg => false, 
+      :is_best_offer => true, 
+      :path_to_image => '/img/fruit_salad.jpg'
+      })
+
+    Product.create ({
       :category => 'drink',
       :title => 'Coca-Cola', 
       :description => 'Classic Coca-Cola', 
@@ -80,6 +110,26 @@ class AddProducts < ActiveRecord::Migration
       :is_veg => true, 
       :is_best_offer => true, 
       :path_to_image => '/img/cola.jpg'
+      })
+
+    Product.create ({
+      :category => 'drink',
+      :title => 'Fanta', 
+      :description => 'Classic Fanta drink', 
+      :price => 1, 
+      :is_veg => true, 
+      :is_best_offer => true, 
+      :path_to_image => '/img/fanta.jpg'
+      })
+
+    Product.create ({
+      :category => 'drink',
+      :title => 'Sprite', 
+      :description => 'Lemon Sprite', 
+      :price => 1, 
+      :is_veg => true, 
+      :is_best_offer => true, 
+      :path_to_image => '/img/sprite.jpg'
       })
 
   end
